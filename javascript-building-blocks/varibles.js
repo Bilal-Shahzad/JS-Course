6970003546082111a8a5fb543b78a2bfa79fb26d
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-25 13:38:00 -0500

    going over varaibles basics and when to use them

---
+++ javascript-building-blocks/varibles.js
@@ -0,0 +1,42 @@
+//4 Ways to Declare a JavaScript Variable
+// Using var, let, const, or nothing
+
+// Variables are containers for storing data 
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log (z)
+
+// x, y, and z, are variables, declared with the var keyword
+// they can be used the same way as the other varaible
+
+// x, y, and z, are variables, declared with the let keyword
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// x, y, and z, are undeclared variables:
+x = 5;
+y = 6;
+z = x + y;
+
+
+// x stores the value 5, y stores the value 6, z stores the value 11
+
+
+// When to Use JavaScript var
+// Always declare JavaScript variables with var,let, orv const.
+
+//EX 1
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+
+// The two variables price1 and price2 are declared with the const keyword.
+// These are constant values and cannot be changed.
+// The variable total is declared with the let keyword. SOmething that can be changed
+
+
+
+
