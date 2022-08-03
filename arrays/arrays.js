0033605dcb5dfdb3b5c7195bd799bf034a4d6f1c
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-08-03 10:22:44 -0500

    start of arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,23 @@
+// An array can hold many values under a single name, and you can access the values by referring to an index number.
+
+const array_name = [item1, item2, ...];      
+
+// declare array with const
+
+const cars = ["Saab", "Volvo", "BMW"];
+// or
+const cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+  
+// You can also create an array, and then provide the elements:
+const cars = [];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+
+// Ex: creates an Array, and assigns values to it:
+const cars = new Array("Saab", "Volvo", "BMW");
