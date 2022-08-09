22e737ee0384f21d2d6cfc0d24e194214555130e
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-08-09 13:52:57 -0500

    finishing up operators and addition of mini apps project

--- javascript-building-blocks/operators2.js
+++ javascript-building-blocks/operators2.js
@@ -1,7 +1,7 @@
 // Arithmetic Operators
 // Arithmetic operators are used to perform mathematical operations between numeric operands.
 
-// Operator	Description
+// Operator - Description
 // +	Adds two numeric operands.
 // -	Subtract right operand from left operand
 // *	Multiply two numeric operands.
@@ -10,3 +10,20 @@
 // ++	Increment operator. Increase operand value by one.
 // --	Decrement operator. Decrease value by one.
 
+var a = 5, b = "Hello ", c = "World!", d = 10;
+
+
+
+// JavaScript provides comparison operators that compare two operands and return a boolean value true or false
+
+// Operators - Description
+// ==	Compares the equality of two operands without considering type.
+// ===	Compares equality of two operands with type.
+// !=	Compares inequality of two operands.
+// >	Returns a boolean value true if the left-side value is greater than the right-side value; otherwise, returns false.
+// <	Returns a boolean value true if the left-side value is less than the right-side value; otherwise, returns false.
+// >=	Returns a boolean value true if the left-side value is greater than or equal to the right-side value; otherwise, returns false.
+// <=	Returns a boolean value true if the left-side value is less than or equal to the right-side value; otherwise, returns false.
+
+var a = 5, b = 10, c = "5";
+var x = a;
