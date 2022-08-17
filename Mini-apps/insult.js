34b0d2431455ba076dab216f5070498412ed8504
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-08-17 17:10:10 -0500

    assinging math.floor and math.random to choos a random item

--- Mini-apps/insult.js
+++ Mini-apps/insult.js
@@ -6,5 +6,10 @@ console.log(randomBodyParts)
 var randomAdjectives = ['Trash', 'Terrible', 'Stupid', 'Dumb'];
 console.log(randomAdjectives)
 
+// ramdom words
 var randomWords = ['Helmet', 'Fan', 'Laptop', 'Pillow', 'Bag'];
 console.log(randomWords)
+
+// choose a random item from `randomBodyParts` array and assign to `randomBodyPart`
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 4)];
+console.log(randomBodyPart)
