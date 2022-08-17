3efcac57fdf9b3eebf2af59ac8dad5c755e98f13
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-08-17 16:49:36 -0500

    randomWords added

--- Mini-apps/insult.js
+++ Mini-apps/insult.js
@@ -1,6 +1,10 @@
 // assigning body parts 
 var randomBodyParts = ['Stomach', 'Legs', 'Face', 'Back'];
+console.log(randomBodyParts)
 
 // givinng adjectives 
 var randomAdjectives = ['Trash', 'Terrible', 'Stupid', 'Dumb'];
+console.log(randomAdjectives)
 
+var randomWords = ['Helmet', 'Fan', 'Laptop', 'Pillow', 'Bag'];
+console.log(randomWords)
