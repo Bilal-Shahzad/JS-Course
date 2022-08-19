478508283b19fe29d741cdc04dad12b23452bf20
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-08-19 13:46:34 -0500

    start of functions

---
+++ functions/functions.js
@@ -0,0 +1,19 @@
+// Function parameters are listed inside the parentheses () in the function definition.
+
+// Function arguments are the values received by the function when it is invoked.
+
+// EX
+
+function name(parameter1, parameter2, parameter3) {
+    // code to be executed
+  }
+
+
+//   Function Return
+// When JavaScript reaches a return statement, the function will stop executing.
+
+let x = myFunction(4, 3);  // Function is called, return value will end up in x
+function myFunction(a, b) 
+console.log(myFunction) 
+return a * b;  // Function returns the product of a and b
+
