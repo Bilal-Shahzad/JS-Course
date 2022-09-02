32093c2af6de1d22cac07e8a99beeb21eb75ddc8
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-09-01 23:37:19 -0500

    calorie app finished

--- Mini-apps/calorie.js
+++ Mini-apps/calorie.js
@@ -7,3 +7,49 @@ function inputCaloriesByDay(day) {
       return 1500;
     } else if (day === 'Wednesday') {
         return 1800;
+    } else if (day === 'Thursday') {
+        return 2300;
+      } else if (day === 'Friday') {
+        return 2400;
+      } else if (day === 'Saturday') {
+        return 1500;
+      } else if (day === 'Sunday') {
+        return 1500;
+      } else {
+        return 'Please choose valid day';
+      }
+}
+    // returns the value of total calories consumed in a week
+    function getTotalCalories() {
+      return inputCaloriesByDay('Monday') +
+      inputCaloriesByDay('Tuesday') +
+      inputCaloriesByDay('Wednesday') +
+      inputCaloriesByDay('Thursday') +
+      inputCaloriesByDay('Friday') +
+      inputCaloriesByDay('Saturday') + 
+      inputCaloriesByDay('Sunday');
+    }
+    
+    // return value of total ideal calories consumed in a week
+    function getIdealCalories() {
+      var idealDailyCalories = 2000;
+      return idealDailyCalories * 7;
+    }
+    
+    // check if you ate equal to, more or less than you should have and
+    // respond with what the person should do more of next week
+    function calculateHealthPlan() {
+      var actualCalories = getTotalCalories();
+      var idealCalories = getIdealCalories();
+      if (actualCalories === idealCalories) {
+        return 'You ate just the right amount!';
+      }
+      if (actualCalories > idealCalories) {
+        return 'Time to head to the gym!';
+      } else {
+        return 'Time for seconds!';
+      }
+    }
+    
+    console.log(calculateHealthPlan());
+    
